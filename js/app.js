3da1535153d7fa7abfff54b3f1e26d796ba6394e
Juli <juliazubova@seznam.cz>
2016-11-26 00:51:32 +0100

    ang1

--- js/app.js
+++ js/app.js
@@ -7,16 +7,19 @@ angular.module('LunchCheck',[])
 
 LunchCheckController.$inject = ['scope'];
 function LunchCheckController($scope) {
-    $scope.dishes="";
-});
-$scope.feedDishes = function() {
-    $count = ($scope.dishes.split(",").length - 1)
-    if ( $count == 0) {
-        $scope.myMessage = 'Please enter data first';
-    } elseif ( $count =< 3) {
-        $scope.myMessage = 'Enjoy!';
-    } else {
-        $scope.myMessage = 'Too much!';
+    $scope.dishes="sss,ddd";
+    $scope.myMessage="";
+
+    $scope.feedDishes = function() {
+        $count = ($scope.dishes.split(",").length - 1)
+        if ( $count == 0) {
+            $scope.myMessage = 'Please enter data first';
+        } else if ( $count <= 3) {
+            $scope.myMessage = 'Enjoy!';
+        } else {
+            $scope.myMessage = 'Too much!';
+        }
     }
 }
+
 })();
